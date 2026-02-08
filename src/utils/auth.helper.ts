cc58cfa63487a27b1cbb23bc40742e144ba7bca3
Shivam Sahu <shivamsahu7770@gmail.com>
2026-02-08 23:01:50 +0530

    auth helper

--- src/utils/auth.helper.ts
+++ src/utils/auth.helper.ts
@@ -11,3 +11,11 @@ export const generateAccessToken = async (payload: { userId: number; email: stri
   // Accessing the internal signer provided by Elysia's JWT plugin
   return await authJwt.signer.sign(payload);
 };
+
+export const hashPassword = async (password: string) => {
+  return await Bun.password.hash(password);
+};
+
+export const comparePassword = async (password: string, hash: string) => {
+  return await Bun.password.verify(password, hash);
+};
