d2942b7ec0f90c7fcd3a22527be49198b739aa4a
Shivam Sahu <shivamsahu7770@gmail.com>
2026-02-10 00:42:45 +0530

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Elysia } from 'elysia';
 import { swagger } from '@elysiajs/swagger';
+import { cors } from '@elysiajs/cors';
 import { adminRoutes } from './routes/admin';
+import { googleAuthRoutes } from './routes/google-auth.route';
+import { reviewRoutes } from './routes/review.route';
 
 const app = new Elysia()
+  .use(cors())
   // 1. Setup Swagger
   .use(
     swagger({
@@ -18,6 +22,14 @@ const app = new Elysia()
   .group('/api/v1', (app) =>
     app.use(adminRoutes)
   )
+  .group('/api/v1', (app) =>
+    app.use(googleAuthRoutes)
+  )
+  .group('/api/v1', (app) =>
+    app.use(reviewRoutes)
+  )
+
+
   // 3. Start Server
   .listen(process.env.PORT || 3000);
 
